bd6c2999dda621d5f20202617d3b53660b1dc4f6
Arshdeep <harkiratkaur8c6@gmail.com>
2025-10-28 21:52:29 +0530

    Added Linkedin Icon correctly in intro file

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,25 +1,30 @@
-import './Intro.css'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faLinkedin } from "@fortawesome/free-solid-svg-icons";
+import './Intro.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"; // ✅ Corrected
 
-function Intro({tagline, intro}){
-   return(
-    <div className = "intro">
-      <div className= "basic">
+function Intro({ tagline, intro }) {
+  return (
+    <div className="intro">
+      <div className="basic">
         <h1>{tagline}</h1>
         <p>{intro}</p>
         <div className="buttons">
-            <a class="get" href="#" >Get In Touch</a>
-            <a class="social" href="">
-               
-            </a>
+          <a className="get" href="#">Get In Touch</a>
+          <a
+            className="social"
+            href="https://www.linkedin.com/in/yourprofile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
         </div>
       </div>
-      <div className = "image">
-        <img src='/Profile Pic.jpg'></img>
+      <div className="image">
+        <img src="/Profile Pic.jpg" alt="Profile" />
       </div>
     </div>
-   )
+  );
 }
 
-export default Intro
+export default Intro;
